9e9f72023251c9913ee4519094220c5e8f870734
Antônio Izaias <antonioizaiasgn@gmail.com>
2021-08-22 22:26:27 -0300

    Formulários e SendGrid configurados

--- src/script.js
+++ src/script.js
@@ -17,3 +17,12 @@ if (window.SimpleSlide) {
 if (window.SimpleAnime) {
     new SimpleAnime();
 }
+
+if (window.SimpleForm) {
+    new SimpleForm({
+        form: ".formphp", // seletor do formulário
+        button: "#enviar", // seletor do botão
+        erro: "<div id='form-erro'><h2>Erro</h2><p>Um erro ocorreu, por favor tente novamente mais tarde.</p></div>", // Mensagem de erro
+        sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // Mensagem de sucesso
+    });
+}
